82ab36c3c33221c3146a3202738a0807801df5b0
vasile <elsile69@yahoo.com>
2018-11-15 12:02:54 +0200

    New configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 var webpack = require('webpack');
-        var path = require('path');
-        var BUILD_DIR = path.join(__dirname, 'dist');
-        var APP_DIR = path.join(__dirname, 'src');
-        var config = {
-          entry: APP_DIR + '/app.js',
-          output: {
-            path: BUILD_DIR,
-            filename: 'app.bundle.js'
-          }
-        }
-        module.exports = config;
+var path = require('path');
+
+var BUILD_DIR = path.join(__dirname, 'dist');
+var APP_DIR = path.join(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/app.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js | jsx)?/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
+module.exports = config;
