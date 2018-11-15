c0e5039a2847951758c40c725ba9c8c007953def
vasile <elsile69@yahoo.com>
2018-11-15 16:10:34 +0200

    Reduce to essential

--- shell.nix
+++ shell.nix
@@ -21,39 +21,5 @@ stdenv.mkDerivation rec {
       yarn init
     fi
 
-    echo "=> Make src directory"
-    if [ -d "src" ]
-    then
-      echo "=> src directory exists"
-    else
-      mkdir -p src
-    fi
-
-    echo "=> Create js file"
-    if [ -f "src/app.js" ]
-    then
-      echo "=> app.js file exists"
-    else
-      echo "console.log('Hello world ... from Webpack')" > src/app.js
-    fi
-
-    echo "=> Create Webpack configuration file"
-    if [ -f "webpack.config.js" ]
-    then
-      echo "webpack.config.js exists"
-    else
-      echo "var webpack = require('webpack');
-            var path = require('path');
-            var BUILD_DIR = path.join(__dirname, 'dist');
-            var APP_DIR = path.join(__dirname, 'src');
-            var config = {
-              entry: APP_DIR + '/app.js',
-              output: {
-                path: BUILD_DIR,
-                filename: 'app.bundle.js'
-              }
-            }
-            module.exports = config;" > webpack.config.js
-    fi
   '';
 }
