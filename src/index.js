3a115708d36acadf380a1c33fc945e8125a23896
vasile <elsile69@yahoo.com>
2018-11-16 16:44:33 +0200

    Create configuration from webpack config tool

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import App from "./App";
-import MyComponent from "./MyComponent";
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.join(__dirname, 'dist');
-var APP_DIR = path.join(__dirname, 'src');
-
-var config = {
-  entry: APP_DIR + '/index.js',
+const config = {
+  entry: './src/index.js',
   output: {
-    path: BUILD_DIR,
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
   }
 }
+
 module.exports = config;
